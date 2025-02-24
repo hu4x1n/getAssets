31af298e35140e70f7dbec35f8121f89d8a7f586
hu4x1n <1242126561@qq.com>
2025-02-24 09:26:10 +0800

    更新代码

--- TZHWtarget.py
+++ TZHWtarget.py
@@ -1,12 +1,13 @@
 # _*_ coding:utf-8 _*_
 # @Time : 2023/6/3 20:57
-# @Author: zhxknb1
+# @Author: hu4x1n
 
 
 import requests
 import openpyxl
 import time
 
+# 修改 1
 header = {
     "Host": "nsiscp.jscert.cn:8078",
     "Connection": "close",
@@ -17,11 +18,12 @@ header = {
     "Cookie": "assetreport_session=eyJpdiI6ImxkUWRHS3BpTHQwWmVXanMzL0NubUE9PSIsInZhbHVlIjoiYlJCcGlFN255NTFmN1V0clI1QmlVNlE1OTh5eHZzT3dtVE1EY0VMZm9UcS83VFk4MDYrYjlNeHJTZ09vekF5UWlaampvNG0xbUNOMWRWZnk0OTIrMHhSMEJZZENyaEgwN3BncmRZNEpsNElOenYycW5OanBUVnFlKzNkZ2x5dHAiLCJtYWMiOiJmMmQ5NDI5OGFmNTg3MTk1YjI1NDk3NzMxN2QzNTJmYTdmNzEwMTQxNTgyNTA2OGYyYTUxYWJjOTgxNzhiMTEwIiwidGFnIjoiIn0%3D",
 }
 page = 1
+# 修改 2
 url = "https://nsiscp.jscert.cn:8078/api/activity/B531B7A7A12BE88C77D94527C8CFB0EB/asset/white_list"
 
-# 在某个文件目录下创建一个excel文件，文件名为TZHWdata.xlsx
-workbook = openpyxl.load_workbook("/Users/zhxknb1/Desktop/TZHWdata.xlsx")
-worksheet = workbook["sheet1"]
+# 在当前文件目录下创建一个excel文件，文件名为TZHWdata.xlsx
+workbook = openpyxl.load_workbook("./TZHWdata.xlsx")
+worksheet = workbook["Sheet1"]
 worksheet["A1"] = "assetUrl"
 worksheet["B1"] = "realIp"
 worksheet["C1"] = "eventNum"
@@ -73,6 +75,6 @@ for n in range(1, 30):
 
     print(f"第{n}页爬取完成")
     # 修改保存路径
-    workbook.save("/Users/zhxknb1/Desktop/TZHWdata.xlsx")
+    workbook.save("./TZHWdata.xlsx")
 
 workbook.close()
